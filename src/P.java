5275ceb6e6d68b1f49e83c4847adf208a211db54
HP <shubhi1488@gmail.com>
2021-09-25 23:50:14 +0530

    Hello

---
+++ src/P.java
@@ -0,0 +1,8 @@
+public class P {
+    public static void main(String[] args){
+        int[] values={1,2,3,4,5,6};
+        for (int i=0;i<values.length;i++){
+            System.out.println(values[i]);
+        }
+    }
+}
